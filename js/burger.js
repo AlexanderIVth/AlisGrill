f00cf074c9dc71670cbfb58ae37343687cf3d6d2
Alexander Parcasio IV <parcasioalexander@outlook.com>
2019-05-02 21:02:04 -0600

    Add files via upload

---
+++ js/burger.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function() {
+  var body = document.body;
+  var burger = document.getElementById('burger');
+  var burgerContain = document.getElementById('burgerNav');
+
+  burger.addEventListener('click', function toggleClasses() {
+    [burgerContain].forEach(function(el) {
+      el.classList.toggle('open');
+    });
+  }, false);
+})();
